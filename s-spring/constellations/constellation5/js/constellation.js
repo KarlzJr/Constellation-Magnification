7a171d137bf13a932ab49869c40c779bc7c12ebf
KarlzJr <67616441+KarlzJr@users.noreply.github.com>
2022-05-20 15:53:46 -0700

    adding constellation folders

---
+++ s-spring/constellations/constellation5/js/constellation.js
@@ -0,0 +1,4 @@
+// Summary: Slider
+// Description: Loops through images on the website
+// @author Alejandra Sicairos and @credits - https://www.youtube.com/watch?v=KkzVFB3Ba_o
+// @created  5.20.22
